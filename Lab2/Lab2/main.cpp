75d4ae730be6d7d7912c72fdef78eaec689b838b
dschulz474 <dschulz474@student.glendale.edu>
2018-05-08 14:00:08 -0700

    error detection

--- Lab2/Lab2/main.cpp
+++ Lab2/Lab2/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 bool IsOperator(char C);
 bool IsOperand(char c);
 int Precedence(char op);
+bool validEXP(string exp);
 
 int main()
 {
@@ -16,7 +17,13 @@ int main()
 	string postfixExp = "";
 	cout << "Enter an infix experssion:	" << endl;
 	cin >> expression;
-	for(int i = 0; i < expression.size(); i++)
+
+	while (validEXP(expression) != true)
+	{
+		cout << "Not a valid infix expression" << endl;
+		cin >> expression;
+	}
+	for(int i = 0; i < expression.length(); i++)
 	{
 		if (IsOperand(expression[i]))
 		{
@@ -32,10 +39,29 @@ int main()
 		{
 			while (!stack.isEmpty() && stack.peek() != '(' && Precedence(expression[i]) <= Precedence(stack.peek()))
 			{
-
+				postfixExp += stack.peek();
+				stack.pop();
 			}
+			stack.push(expression[i]);
+		}
+		else if (expression[i] == ')')
+		{
+			while (stack.peek() != '(')
+			{
+				postfixExp += stack.peek();
+				stack.pop();
+			}
+			stack.pop();
 		}
 	}
+
+	while (!stack.isEmpty())
+	{
+		postfixExp += stack.peek();
+		stack.pop();
+	}
+
+	cout << "Postfix: "<< postfixExp << endl;
 	system("pause");
 	return 0;
 }
@@ -70,3 +96,65 @@ int Precedence(char op1)
 	return weight;
 }
 
+bool validEXP(string expression)
+{
+	char prev = ' ';
+	string bracketCheck = "";
+	for (int i = 0; i < expression.length(); i++)
+	{
+		if (i > 0)
+		{
+			prev = expression[i - 1];
+		}
+		if (prev == expression[i])
+		{
+			return false;
+		}
+		if (expression[i] == '(')
+		{
+			bracketCheck += expression[i];
+		}
+		if (expression[i] == ')')
+		{
+			bracketCheck += expression[i];
+		}
+	}
+	if (bracketCheck.size() % 2 != 0)
+	{
+		return false;
+	}
+		
+for (int j = 0; j < bracketCheck.length(); j++)
+{
+	bool leftBracket = false;
+	bool rightBracket = false;
+	if (bracketCheck[0] != '(')
+	{
+		return false;
+	}
+	if (bracketCheck[bracketCheck.length()] != ')')
+	{
+		return false;
+	}
+	if (j > 0)
+	{
+		if (bracketCheck[j - 1] == '(')
+		{
+			leftBracket = true;
+		}
+		else
+		{
+			return leftBracket;
+		}
+
+		if (bracketCheck[j] == ')')
+		{
+			rightBracket = true;
+		}
+		else
+		{
+			return rightBracket;
+		}
+	}
+}
+}
