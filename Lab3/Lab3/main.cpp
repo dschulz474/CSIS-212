9be0738fb53483907b277141424ba08acf540af7
dschulz474 <dschulz474@student.glendale.edu>
2018-05-14 21:04:37 -0700

    no message

--- Lab3/Lab3/main.cpp
+++ Lab3/Lab3/main.cpp
@@ -8,24 +8,40 @@
 #include <string>
 using namespace std;
 
-bool isaNumber(char c);
-bool isaLetter(char c);
-bool isSpecial(char c);
+bool isaNumber(string c);
+bool isaLetter(string c);
+bool isSpecial(string c);
 void Radix();
-string GenerateString();
+string generateChar();
 int main()
 {
-	srand(time(NULL));
-	int digitCount, specialCount, charCount;
-	
+	srand((unsigned)time(NULL));
 	string queues[10000];
+	int letterCount = 0;
+	int digitCount = 0;
+	int specialCount = 0;
+	int letterLimit = 10000 * (8 / 10);
+	int digitLimit = 10000 * (1 / 10);
+	int specialLimit = 10000 * (1 / 10);
 	
+
+
 	for (int i = 0; i < 10000; i++)
 	{
-		queues[i] = GenerateString();
+		
+		string temp2;
+		int stringLength = rand() % 15 + 1;
+		for (int j = 0; j < stringLength; j++)
+		{
+			string temp = "";
+			temp = generateChar();
+			
+			temp2 += temp;
+		}
+		queues[i] = temp2;
+		
 	}
-	cout << queues->size() << endl;
-	cout << "Printing Array" << endl;
+	cout << "Printing array" << endl;
 	for (int i = 0; i < 10000; i++)
 	{
 		cout << queues[i] << endl;
@@ -37,44 +53,53 @@ int main()
 
 }
 
-string GenerateString()
+string generateChar()
 {
-	string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
-	string temp = "";
-	int stringlength = rand() % 15 + 1;
-	int randomCharacter = rand() % (characters.length()-1) + 0;
-	for (int i = 0; i < stringlength; i++)
+	string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!";
+	string numbers = "0123456789";
+	string special = "@#$%^&*";
+	string temp;
+	int randomChar = rand() % 10 + 1;
+	if (randomChar <= 8)
 	{
-		temp += characters[randomCharacter];
+		temp = characters[rand() % (numbers.length()-1) + 0];
 	}
+	else if (randomChar == 9)
+	{
+		temp = numbers[rand() % (numbers.length() - 1) + 0];
+	}
+	else
+	{
+		temp = special[rand() % (special.length() - 1) + 0];
+	}
+	
 	return temp;
 }
-
 void Radix()
 {
-	LinkedQueue<char> queues[95];
+
 }
 
-bool isaLetter(char c)
+bool isaLetter(string c)
 {
-	if (c >= 'a' || c <= 'z' || c >= 'A' || c<= 'Z')
+	if (c >= "a" || c <= "z" || c >= "A" || c <= "Z")
 	{
 		return true;
 	}
 	else return false;
 }
 
-bool isSpecial(char c)
+bool isSpecial(string c)
 {
-	if (c == '!' || c == '@' || c == '#' || c == '$' || c == '%' || c == '^' || c == '&' || c == '*')
+	if (c == "!" || c == "@" || c == "#" || c == "$" || c == "%" || c == "^" || c == "&" || c == "*")
 	{
 		return true;
 	}
 	else return false;
 }
-bool isaNumber(char c)
+bool isaNumber(string c)
 {
-	if (c >= '0' || c <= '9')
+	if (c >= "0" || c <= "9")
 	{
 		return true;
 	}
